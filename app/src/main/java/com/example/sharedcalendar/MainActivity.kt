3c64047106e6defe3c7a884102a68f96d7663a27
hyngav <125797874+hyngav@users.noreply.github.com>
2024-04-15 11:45:38 +0800

    completed sidebar

--- app/src/main/java/com/example/sharedcalendar/MainActivity.kt
+++ app/src/main/java/com/example/sharedcalendar/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.example.sharedcalendar
 
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
 //    private val TAG = "MainActivity"
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
+        var buttonDrawerToggle : ImageButton = findViewById(R.id.drawerLayoutToggle)
+
+        buttonDrawerToggle.setOnClickListener({
+            drawerLayout.open()
+        })
     }
 }
